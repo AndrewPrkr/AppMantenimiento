37a320e73104499c6f548e87b884871dcba5944d
AndrewPrkr <saeoliand@gmail.com>
2025-09-16 17:38:02 -0600

    login component

--- ProyectoPrueba/src/app/app.ts
+++ ProyectoPrueba/src/app/app.ts
@@ -3,26 +3,15 @@ import { Router, RouterOutlet } from '@angular/router';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Servicedate } from './services/servicedate';
+import { Logincomponent } from "./components/logincomponent/logincomponent";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, Logincomponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
-  
-    constructor(private fecha: Servicedate) {}
-    
-    date!: Observable<Date>;
-  
-    ngOnInit(){
-     this.date = this.fecha.currentTime$; // ✅ observable directo
-  }
-
-
-
   }
 
 
