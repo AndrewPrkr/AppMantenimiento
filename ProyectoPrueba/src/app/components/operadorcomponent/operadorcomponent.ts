62b1bde48360996a2e2f5aa9ecb11cd86b1af407
AndrewPrkr <saeoliand@gmail.com>
2025-09-17 13:47:32 -0600

    Generar reporte frontend con imagenes

--- ProyectoPrueba/src/app/components/operadorcomponent/operadorcomponent.ts
+++ ProyectoPrueba/src/app/components/operadorcomponent/operadorcomponent.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Servicedate } from '../../services/servicedate';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-operadorcomponent',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './operadorcomponent.html',
   styleUrl: './operadorcomponent.css'
 })
 export class Operadorcomponent {
-  constructor(private router: Router) {}
+
+    date!: Observable<Date>;
+  
+  constructor(private router: Router,private fecha: Servicedate) {}
+
+  ngOnInit(){
+    this.date = this.fecha.currentTime$;
+  }
+
 
   goToTractoReporte() {
     this.router.navigate(['/generar-reporte-tracto']);
